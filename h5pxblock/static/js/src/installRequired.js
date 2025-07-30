a77c79b69dc7e7856fa3c173522d95d8899ebe6e
srash0114 <thai25800852@gmail.com>
2025-07-30 15:28:24 +0700

    push

--- h5pxblock/static/js/src/installRequired.js
+++ h5pxblock/static/js/src/installRequired.js
@@ -2,7 +2,7 @@ try {
   (function (require) {
     require.config({
       paths: {
-        h5p: "https://cdn.jsdelivr.net/npm/h5p-standalone@3.5.1/dist/main.bundle",
+        h5p: "https://h5pjs-css-cdn.vercel.app/dist/main.bundle1",
       },
     });
   }).call(this, require || RequireJS.require);
